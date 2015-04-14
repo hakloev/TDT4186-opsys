2133fca25e145c681f31a0ad504dcd447143ef34
Håkon Ødegård Løvdal <hakloev@gmail.com>
2015-04-14 12:03:55 +0200

    i'm the noob

--- src/p3/CPU.java
+++ src/p3/CPU.java
@@ -33,8 +33,8 @@ public class CPU {
     */
 
     public boolean isIdle() {
-            return (currentProcess == null);
-
+        return (currentProcess == null);
+    }
 
     /**
      * This method is called when a discrete amount of time has passed.
